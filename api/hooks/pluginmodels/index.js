205203626891427f2d61297d41cbd5446d6f19d8
kumpelblase2 <tim@infinityblade.de>
2015-01-07 10:33:27 +0100

    Wait for plugin loader when loading models.

--- api/hooks/pluginmodels/index.js
+++ api/hooks/pluginmodels/index.js
@@ -1,7 +1,8 @@
 module.exports = function(sails) {
     return {
         initialize: function(cb) {
-            sails.after('hook:app:loaded', function() {
+            var waitFor = ['hook:app:loaded', 'hook:pluginloaded:loaded'];
+            sails.after(waitFor, function() {
                 sails.log.verbose('Loading custom plugin models...');
                 sails.app.customModels.forEach(function(model) {
                     sails.models[model.name] = model.shema;
