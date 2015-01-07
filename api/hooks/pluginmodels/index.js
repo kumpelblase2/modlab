9c101e6b9310336d885683b876e386b1bd0ad961
kumpelblase2 <tim@infinityblade.de>
2015-01-07 10:34:07 +0100

    Typo

--- api/hooks/pluginmodels/index.js
+++ api/hooks/pluginmodels/index.js
@@ -1,7 +1,7 @@
 module.exports = function(sails) {
     return {
         initialize: function(cb) {
-            var waitFor = ['hook:app:loaded', 'hook:pluginloaded:loaded'];
+            var waitFor = ['hook:app:loaded', 'hook:pluginloader:loaded'];
             sails.after(waitFor, function() {
                 sails.log.verbose('Loading custom plugin models...');
                 sails.app.customModels.forEach(function(model) {
