3498bd86676c2317ab12a5375f51c3b5a329e06a
kumpelblase2 <tim@infinityblade.de>
2015-01-07 11:29:16 +0100

    Don't log main app load status by default.

--- api/hooks/app/index.js
+++ api/hooks/app/index.js
@@ -1,9 +1,9 @@
 module.exports = function(sails) {
     return {
         initialize: function(cb) {
-            sails.log.info('loaded app');
             var App = require('../../app');
             sails.app = new App();
+            sails.log.verbose('Loaded main app');
             cb();
         }
     };
