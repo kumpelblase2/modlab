df164b4570893a08c20642df77b1841fe71479bb
kumpelblase2 <tim@infinityblade.de>
2015-01-08 10:20:54 +0100

    Promisify sails in global scope.

--- api/hooks/app/index.js
+++ api/hooks/app/index.js
@@ -1,6 +1,10 @@
+var Promise = require('bluebird');
+
 module.exports = function(sails) {
     return {
         initialize: function(cb) {
+            global['sails'] = Promise.promisifyAll(sails);
+
             var App = require('../../app');
             sails.app = new App();
             sails.log.verbose('Loaded main app');
