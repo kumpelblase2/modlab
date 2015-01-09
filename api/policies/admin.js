bcc2788912d27209eba819fe5c55f399758e0c06
kumpelblase2 <tim@infinityblade.de>
2015-01-09 14:03:23 +0100

    Fix admin policy.

--- api/policies/admin.js
+++ api/policies/admin.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
 
     // User is allowed, proceed to the next policy,
     // or if this is the last policy, the controller
-    if (req.session.authenticated && req.user.hasPermission('user.admin')) {
+    if (req.isAuthenticated() && req.user.hasPermission('user.admin')) {
         return next();
     }
 
