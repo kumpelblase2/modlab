0723657fa98a73627156eeda1dfc68fa912a6c20
kumpelblase2 <tim@infinityblade.de>
2015-01-09 14:04:55 +0100

    Finished installation first step.

--- api/controllers/install/StepsController.js
+++ api/controllers/install/StepsController.js
@@ -1,23 +1,36 @@
 module.exports = {
     '1': function(req, res) {
+        if(sails.app.installation.step > 1)
+            return res.notFound();
+
         if(req.method === 'POST') {
             var username = req.param('username');
             var password = req.param('password');
             var email = req.param('email') || 'admin@localhost.com';
 
             RegisterService.register(username, email, password, function(err, user) {
-                console.log(err);
-                console.log(user);
                 if(err) {
                     res.view({
                         errors: [req.__(err.message)]
                     });
                 } else {
-                    res.redirect('/login?redirect=/install/steps/2');
+                    sails.app.installation.step = 2;
+                    sails.app.installation.save().then(function() {
+                        user.addPermission('user.admin');
+                        return user.save();
+                    }).then(function() {
+                        res.redirect('/login?redirect=/install/steps/2');
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
                 }
             });
         } else {
             res.view();
         }
+    },
+
+    '2': function(req, res) {
+        res.view();
     }
 };
