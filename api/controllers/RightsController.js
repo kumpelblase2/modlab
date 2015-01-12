a5f4866c6e0e6238f9d290d30cb6500082b1c47a
kumpelblase2 <tim@infinityblade.de>
2015-01-12 15:09:16 +0100

    Forgot to populate associations.

--- api/controllers/RightsController.js
+++ api/controllers/RightsController.js
@@ -3,7 +3,7 @@ module.exports = {
         if(!req.user.hasPermission('system.rights.view')) {
             res.forbidden('Error.Authorization.NoRights', '403');
         } else {
-            User.find().then(function(users) {
+            User.find().populate('permission_groups').then(function(users) {
                 var minifiedUsers = users.map(function(user) {
                     var tmp = _.pick(user, 'id', 'username', 'permissions');
                     tmp.groups = user.permission_groups.map(function(group) {
@@ -12,7 +12,7 @@ module.exports = {
                     return tmp;
                 });
 
-                PermissionGroup.find().then(function(groups) {
+                PermissionGroup.find().populate('users_in_group').then(function(groups) {
                     var minifiedGroups = groups.map(function(group) {
                         var temp = _.pick(group, 'id', 'name', 'permissions');
                         temp.users = group.users_in_group.length;
