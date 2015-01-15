11a3a79bb4d3c5092d7ad3dcdf5eb6dd360e5e60
Tim Hagemann <tim@infinityblade.de>
2015-01-16 00:16:12 +0100

    Add button to remove group.

--- assets/js/app/rights.js
+++ assets/js/app/rights.js
@@ -2,4 +2,30 @@ $(function() {
     $('#btnAddPermission').click(function() {
         $(this).parent().append(JST["assets/templates/rights/permissioninput.html"]);
     });
+
+    $('#deleteGroup').click(function() {
+        var self = this;
+        swal({
+            title: 'Are you sure?',
+            text: 'If you continue, you will not be able to recover the deleted group.',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#DD6B55',
+            confirmButtonText: 'Yes, continue'
+        }, function() {
+            $.ajax({
+                url: self.getAttribute('data-href'),
+                type: 'DELETE',
+                dataType: 'json',
+                success: function(result) {
+                    window.location.href = self.getAttribute('data-return');
+                },
+                error: function(result) {
+                    if(result.status === 200) {
+                        window.location.href = self.getAttribute('data-return');
+                    }
+                }
+            });
+        });
+    });
 });
