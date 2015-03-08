6e108bca5c8177f486cf9cfb705571df6c633f2b
Tim Hagemann <tim@infinityblade.de>
2015-03-08 11:35:55 +0100

    Disable loading count of users until promises work in widgets.

--- modules/system-info-widgets/WidgetController.js
+++ modules/system-info-widgets/WidgetController.js
@@ -1,8 +1,8 @@
 module.exports = {
     modules: function(req) {
         var contentString = "<div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-3'><i class='fa fa-cog fa-5x'></i>";
-        contentString += "</div><div class='col-xs-9 text-right'><div class='huge'>" + Object.keys(sails.app.modules).length + "</div><div>Enabled plugins</div></div></div></div>";
-        contentString += "<a href='/plugins'><div class='panel-footer'><span class='pull-left'>View</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i>";
+        contentString += "</div><div class='col-xs-9 text-right'><div class='huge'>" + Object.keys(sails.app.modules).length + "</div><div>Enabled modules</div></div></div></div>";
+        contentString += "<a href='/modules'><div class='panel-footer'><span class='pull-left'>View</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i>";
         contentString += "</span><div class='clearfix'></div></div></a></div>";
 
         return {
@@ -12,9 +12,8 @@ module.exports = {
         };
     },
     users: function(req) {
-        console.log(User.count());
         var contentString = "<div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-3'><i class='fa fa-comments fa-5x'></i>";
-        contentString += "</div><div class='col-xs-9 text-right'><div class='huge'>" + User.find().count() + "</div><div>Registered Users</div></div></div></div>";
+        contentString += "</div><div class='col-xs-9 text-right'><div class='huge'>" + "1"/*User.find().count()*/ + "</div><div>Registered Users</div></div></div></div>";
         contentString += "<a href='/rights'><div class='panel-footer'><span class='pull-left'>View</span><span class='pull-right'><i class='fa fa-arrow-circle-right'></i>";
         contentString += "</span><div class='clearfix'></div></div></a></div>";
 
