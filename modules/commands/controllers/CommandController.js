36b3adf8c7ac1b5bf8064fd99966397117e09c6e
Tim Hagemann <tim@infinityblade.de>
2015-05-07 23:56:14 +0200

    Base edits for webinterface

--- modules/commands/controllers/CommandController.js
+++ modules/commands/controllers/CommandController.js
@@ -3,5 +3,31 @@ module.exports = {
         Command.find().exec(function(err, commands) {
             res.view('index', { commands: commands });
         });
+    },
+    create: function(req, res) {
+        var self = this;
+        Command.create({
+            name: req.param('name'),
+            type: req.param('type'),
+            data: req.param('data')
+        }).then(function(command) {
+            req.flash('new_command', command);
+            self.index(req, res);
+        });
+    },
+    update: function(req, res) {
+        Command.find(req.param('id')).then(function(command) {
+            command.name = req.param('name', command.name);
+            command.type = req.param('type', command.type);
+            command.data = req.param('data', command.data);
+            return Command.save(command);
+        }).then(function() {
+            res.redirect('/m/commands/');
+        });
+    },
+    deleteC: function(req, res) {
+        Command.delete(req.param('id')).then(function() {
+            res.redirect('/m/commands/');
+        });
     }
 };
