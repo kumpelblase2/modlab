fb32b85b8389b83c3f9b958c94c9cfb939610a87
Tim Hagemann <tim@infinityblade.de>
2015-06-17 14:58:22 +0200

    Added base for stream service to access API calls.

---
+++ api/services/StreamService.js
@@ -0,0 +1,26 @@
+module.exports = {
+    host: function(inStream) {
+
+    },
+    unhost: function() {
+
+    },
+    getHost: function() {
+
+    },
+    setTitle: function(inTitle) {
+
+    },
+    getTitle: function() {
+
+    },
+    setGame: function(inName) {
+
+    },
+    getGame: function() {
+
+    },
+    getViewers: function() {
+
+    }
+};
